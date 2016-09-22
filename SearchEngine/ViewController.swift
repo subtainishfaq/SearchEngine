c2787855d4387f841b8afbd791212817f7e42fdc
subtainishfaq <Bese-18a-+>
2016-09-23 02:56:34 +0500

    Adds Webview for google

--- SearchEngine/ViewController.swift
+++ SearchEngine/ViewController.swift
@@ -10,9 +10,16 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var GoogleHolder: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string : "http://www.google.com")
+        let request = NSURLRequest (url : url! as URL)
+        
+        GoogleHolder.loadRequest(request as URLRequest)
     }
 
     override func didReceiveMemoryWarning() {
